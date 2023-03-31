20e8dcc831d03c5dd82a3107c732ef9444892830
Don McCasland <donmccasland@google.com>
2023-03-30 22:40:56 -0700

    fix - emit relative path url

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -25,7 +25,7 @@ output "firebase_url" {
 
 output "neos_toc_url" {
   description = "Neos Tutorial URL"
-  value       = "https://console.cloud.google.com/products/solutions/deployments?walkthrough_id=panels--sic--dynamic-python-web-app_toc"
+  value       = "/products/solutions/deployments?walkthrough_id=panels--sic--dynamic-python-web-app_toc"
 }
 
 output "django_admin_url" {

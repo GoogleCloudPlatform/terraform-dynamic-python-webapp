b260df6321e9e2f5e1ac32fdeef00abb6f2ce1e2
Katie McLaughlin <katie@glasnt.com>
2023-04-27 15:53:07 +1000

    fix: ensure api dependence

--- infra/firebase.tf
+++ infra/firebase.tf
@@ -25,4 +25,6 @@ resource "google_firebase_hosting_site" "client" {
   provider = google-beta
   project  = var.project_id
   site_id  = var.random_suffix ? "${var.project_id}-${random_id.suffix.hex}" : var.project_id
+
+  depends_on = [google_project_service.enabled]
 }

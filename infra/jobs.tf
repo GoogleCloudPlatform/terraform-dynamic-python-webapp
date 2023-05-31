168394f405052fdf0f99c2f3cead6c7a026b9880
Katie McLaughlin <katie@glasnt.com>
2023-05-31 13:01:32 +1000

    Add additional inline documenation on envvar usage

--- infra/jobs.tf
+++ infra/jobs.tf
@@ -115,6 +115,9 @@ resource "google_cloud_run_v2_job" "client" {
       service_account = google_service_account.client.email
       containers {
         image = local.client_image
+
+        # Variables used to customise Firebase configuration on deployment
+        # https://github.com/GoogleCloudPlatform/avocano/blob/main/client/docker-deploy.sh
         env {
           name  = "SUFFIX"
           value = var.random_suffix ? random_id.suffix.hex : ""
